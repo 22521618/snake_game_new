72c9dd8763e2562d964ebec5101cc36570c29595
22521618 <22521618@gm.uit.edu.vn>
2024-06-19 22:51:12 +0700

    done

--- lib/snek_buttons/snek_buttons.h
+++ lib/snek_buttons/snek_buttons.h
@@ -1,9 +1,9 @@
 #include <driver/gpio.h>
 
-#define BTN_UP GPIO_NUM_23
-#define BTN_DN GPIO_NUM_5
-#define BTN_L GPIO_NUM_22
-#define BTN_R GPIO_NUM_18
+#define BTN_UP GPIO_NUM_5
+#define BTN_DN GPIO_NUM_23
+#define BTN_L GPIO_NUM_18
+#define BTN_R GPIO_NUM_22
 volatile extern uint8_t button_pressed;
 
 void button_config();
